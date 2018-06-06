fbaa53cf1df95e874d62e9fba405c379f2951829
John Mount <jmount@win-vector.com>
2018-06-06 09:56:19 -0700

    message if can not register self as executor

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,13 @@
 
 
-rqdatatable_default_options <- list(
-  rquery.rquery_executor = list(f = ex_data_table)
-)
 
 .onLoad <- function(libname, pkgname) {
-  op <- options()
-  toset <- setdiff(names(rqdatatable_default_options), names(op))
-  if(length(toset)>0) {
-    options(rqdatatable_default_options[toset])
+  prev_exec <- getOption("rquery.rquery_executor", default = NULL)
+  if(is.null(prev_exec) || (is.list(prev_exec) && isTRUE(prev_exec$name == "rqdatable"))) {
+    options(list("rquery.rquery_executor" = list(f = ex_data_table, name = "rqdatable")))
+  } else {
+    message("rqdatatable loaded, but did not register itself as the executor (already one registered)")
   }
   invisible()
 }
+
