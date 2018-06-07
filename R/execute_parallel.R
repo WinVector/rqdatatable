52c55aad1c7072f8fdfa9551e7a4580d25833387
John Mount <jmount@win-vector.com>
2018-06-07 11:49:02 -0700

    work on parallel

---
+++ R/execute_parallel.R
@@ -0,0 +1,105 @@
+
+
+#' @importFrom parallel clusterApplyLB
+NULL
+
+
+parallel_f <- function(tables, ...) {
+  args <- list(...)
+  optree = args[["optree"]]
+  res <- ex_data_table(optree = optree,
+                       tables = tables)
+  res
+}
+
+
+#' Execute an \code{rquery} pipeline with \code{data.table} in parallel.
+#'
+#' Execute an \code{rquery} pipeline with \code{data.table} in parallel partitioned by a given column.
+#'
+#'
+#' @param optree relop operations tree.
+#' @param partition_column character name of column to partition work by.
+#' @param cl a cluster object, created by package parallel or by package snow. If NULL, use the registered default cluster.
+#' @param ... not used, force later arguments to bind by name.
+#' @param tables named list map from table names used in nodes to data.tables and data.frames.
+#' @param source_limit if not null limit all table sources to no more than this many rows (used for debugging).
+#' @param debug logical if TRUE use lapply instead of parallel::clusterApplyLB.
+#' @param env environment to work in.
+#' @return resulting data.table (intermediate tables can somtimes be mutated as is practice with data.table).
+#'
+#'
+#'
+#' @export
+#'
+ex_data_table_parallel <- function(optree,
+                                   partition_column,
+                                   cl = NULL,
+                                   ...,
+                                   tables = list(),
+                                   source_limit = NULL,
+                                   debug = FALSE,
+                                   env = parent.frame()) {
+  source_usage <- columns_used(optree)
+  # make sure all needed tables are in the ntables list
+  ntables <- list()
+  for(ni in names(source_usage)) {
+    ti <- tables[[ni]]
+    if(is.null(ti)) {
+      ti <- get(ni, envir = env) # should throw if not found
+    }
+    # front-load some error checking
+    if(!is.data.frame(ti)) {
+      stop(paste("rquery::ex_data_table_parallel all arguments must resolve to data.frames",
+           ni, paste(class(ti), collapse = " ")))
+    }
+    nsi <- source_usage[[ni]]
+    nti <- colnames(ti)
+    missing <- setdiff(nsi, nti)
+    if(length(missing)>0) {
+      stop(paste("rquery::ex_data_table_parallel missing required columns",
+                 ni, paste(missing, collapse = " ")))
+    }
+    if((partition_column %in% nti) && (!(partition_column %in% nsi))) {
+      # preserve partition column
+      nsi <- c(nsi, partition_column)
+    }
+    if(is.null(source_limit) || (nrow(ti)>source_limit)) {
+      ntables[[ni]] <- ti[ , nsi, drop = FALSE]
+    } else {
+      ntables[[ni]] <- ti[seq_len(source_limit), nsi, drop = FALSE]
+    }
+  }
+  # get a list of values of the partition column
+  levels <- c()
+  for(ni in names(ntables)) {
+    ti <- ntables[[ni]]
+    if(partition_column %in% colnames(ti)) {
+      levels <- unique(c(levels, ti[[partition_column]]))
+    }
+  }
+  # build a list of tablesets
+  tablesets <- lapply(levels,
+                      function(li) {
+                        nti <- ntables
+                        for(ni in names(ntables)) {
+                          ti <- ntables[[ni]]
+                          if(partition_column %in% colnames(ti)) {
+                            nti[[ni]] <- ti[ti[[partition_column]]==li, , drop = FALSE]
+                          }
+                        }
+                        nti
+                      })
+  if(debug) {
+    res <- lapply(tablesets, parallel_f, optree = optree)
+  } else {
+    # dispatch the operation in parallel
+    res <- parallel::clusterApplyLB(cl, tablesets, parallel_f, optree = optree)
+  }
+  res <- data.table::rbindlist(res)
+  if("relop_orderby" %in% class(optree)) {
+    reord <- orderby(local_td(res), cols = optree$orderby, reverse = optree$reverse)
+    res <- ex_data_table(optree = reord)
+  }
+  res
+}
