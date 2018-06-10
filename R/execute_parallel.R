32a24ba357ffa09c8e10703e40db064ee84c71cc
John Mount <jmount@win-vector.com>
2018-06-10 14:49:34 -0700

    move to wrapr::partition_tables

--- R/execute_parallel.R
+++ R/execute_parallel.R
@@ -13,6 +13,7 @@ parallel_f <- function(tables, ...) {
 }
 
 
+
 #' Execute an \code{rquery} pipeline with \code{data.table} in parallel.
 #'
 #' Execute an \code{rquery} pipeline with \code{data.table} in parallel, partitioned by a given column.
@@ -52,62 +53,12 @@ ex_data_table_parallel <- function(optree,
   }
   wrapr::stop_if_dot_args(substitute(list(...)), "rqdatatable::ex_data_table_parallel")
   source_usage <- columns_used(optree)
-  # make sure all needed tables are in the ntables list
-  ntables <- list()
-  for(ni in names(source_usage)) {
-    ti <- tables[[ni]]
-    if(is.null(ti)) {
-      ti <- get(ni, envir = env) # should throw if not found
-    }
-    # front-load some error checking
-    if(!is.data.frame(ti)) {
-      stop(paste("rqdatatable::ex_data_table_parallel all arguments must resolve to data.frames",
-           ni, paste(class(ti), collapse = " ")))
-    }
-    nsi <- source_usage[[ni]]
-    nti <- colnames(ti)
-    missing <- setdiff(nsi, nti)
-    if(length(missing)>0) {
-      stop(paste("rqdatatable::ex_data_table_parallel missing required columns",
-                 ni, paste(missing, collapse = " ")))
-    }
-    if((partition_column %in% nti) && (!(partition_column %in% nsi))) {
-      # preserve partition column
-      nsi <- c(nsi, partition_column)
-    }
-    if(is.null(source_limit) || (nrow(ti)>source_limit)) {
-      ntables[[ni]] <- ti[ , nsi, drop = FALSE]
-    } else {
-      ntables[[ni]] <- ti[seq_len(source_limit), nsi, drop = FALSE]
-    }
-  }
-  env <- NULL
-  tables <- NULL
-  # get a list of values of the partition column
-  levels <- c()
-  for(ni in names(ntables)) {
-    ti <- ntables[[ni]]
-    if(partition_column %in% colnames(ti)) {
-      levels <- unique(c(levels, as.character(ti[[partition_column]])))
-    }
-  }
-  if(length(levels)<=0) {
-    stop(paste("rqdatatable::ex_data_table_parallel no values found for partition column", partition_column))
-  }
-  # build a list of tablesets
-  tablesets <- lapply(levels,
-                      function(li) {
-                        nti <- ntables
-                        for(ni in names(ntables)) {
-                          ti <- ntables[[ni]]
-                          if(partition_column %in% colnames(ti)) {
-                            ti <- ti[as.character(ti[[partition_column]])==li, , drop = FALSE]
-                          }
-                          nti[[ni]] <- as.data.frame(ti)
-                        }
-                        nti
-                      })
-  ntables <- NULL
+  tablesets <- wrapr::partition_tables(names(source_usage),
+                                       partition_column = partition_column,
+                                       source_usage = source_usage,
+                                       source_limit = source_limit,
+                                       tables = tables,
+                                       env = env)
   if(debug) {
     res <- lapply(tablesets, parallel_f, optree = optree)
   } else {
