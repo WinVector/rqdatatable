5c2ba1b5b404d9788891e9cca096fc526ff403df
John Mount <jmount@win-vector.com>
2018-09-20 10:59:54 -0700

    dt-factory for column_lookup()

---
+++ R/make_dt_lookup_by_column.R
@@ -0,0 +1,44 @@
+
+#' Lookup by column function factory.
+#'
+#' Build data.table implementation of lookup_by_column.  We do this here as rqdatatable is
+#' a data.table aware package (and rquery is not).
+#'
+#' @param pick character scalar, name of column to control value choices.
+#' @param result character scalar, name of column to place values in.
+#' @return f_dt() function.
+#'
+#' @examples
+#'
+#' library("rqdatatable")
+#'
+#' df = data.frame(x = c(1, 2, 3, 4),
+#'                 y = c(5, 6, 7, 8),
+#'                 choice = c("x", "y", "x", "z"),
+#'                 stringsAsFactors = FALSE)
+#'
+#' # base-R implementation
+#' df %.>% lookup_by_column(., "choice", "derived")
+#' # # data.table implementation (requies rquery 1.1.0, or newer)
+#' # df %.>% lookup_by_column(., "choice", "derived",
+#' #                          f_dt_factory = rqdatatable::make_dt_lookup_by_column)
+#'
+#' @export
+#'
+#' @seealso \code{\link[rquery]{lookup_by_column}}
+#'
+make_dt_lookup_by_column <- function(pick, result) {
+  force(pick)
+  force(result)
+  f_dt <- function(d) {
+    dt <- as.data.table(d)
+    # dt[, RESULT := .SD[[PICK]], by = PICK][] # alternate
+    .I <- PICK <- RESULT <- NULL # don't look like unbound symbols
+    wrapr::let(
+      c(PICK = pick, RESULT = result),
+      dt[, RESULT := dt[[PICK]][.I], by = PICK]
+    )
+    dt[]
+  }
+  f_dt
+}
