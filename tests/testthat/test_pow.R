12d1c02be18d2c8ce81d5ecf248e41fd082f28de
John Mount <jmount@win-vector.com>
2018-11-18 11:47:05 -0800

    move to rquery 1.2.1

--- tests/testthat/test_pow.R
+++ tests/testthat/test_pow.R
@@ -3,8 +3,7 @@ library("rqdatatable")
 context("pow")
 
 test_that("test_pw: Works As Expected", {
-  ## TODO: wait on rquery 1.2.1 for this test
-  # d <- data.frame(x = 2)
-  # d2 <- extend(d, xsq = x^2)
-  # testthat::expect_equivalent(data.frame(x = 2, xsq = 4), data.frame(d2))
+  d <- data.frame(x = 2)
+  d2 <- extend(d, xsq = x^2)
+  testthat::expect_equivalent(data.frame(x = 2, xsq = 4), data.frame(d2))
 })
