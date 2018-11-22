5f9c7bb4691de4825a6ee4bca6ea329e206e2d36
John Mount <jmount@win-vector.com>
2018-11-22 15:40:35 -0800

    update rq_partial

--- R/rq_partial.R
+++ R/rq_partial.R
@@ -1,6 +1,11 @@
 
+
+#' @importFrom wrapr ApplyTo
+#' @importClassesFrom wrapr UnaryFn UnaryFnList PartialNamedFn PartialFunction
+NULL
+
 f_eval_partial_step <- function(d, nd) {
-  wrapr::applyto(nd$partial_step, d)
+  wrapr::ApplyTo(nd$partial_step, d)
 }
 
 
@@ -8,11 +13,8 @@ f_eval_partial_step <- function(d, nd) {
 #' Wrap a function name and arguments as an rqdatatable stage.
 #'
 #' @param source incoming relop pipeline.
-#' @param fn_name character, name of function.
+#' @param step a wrapr::UnaryFn derived S4 instance.
 #' @param ... force later arguments to be taken by name.
-#' @param fn_package character, name of package.
-#' @param arg_name name for remaining argument.
-#' @param args list of function argument values
 #' @param columns_produced columns of this node's result.
 #' @param check_result_details logical, if TRUE enforce result type and columns.
 #' @param env environment to work in.
@@ -22,20 +24,16 @@ f_eval_partial_step <- function(d, nd) {
 #'
 #' @export
 #'
-rq_partial <- function(source, fn_name,
+rq_partial <- function(source, step,
                        ...,
-                       fn_package = "base",
-                       arg_name = '', args = list(),
                        columns_produced = NULL,
                        check_result_details = TRUE,
                        env = parent.frame()) {
   force(columns_produced)
+  force(step)
   force(env)
   wrapr::stop_if_dot_args(substitute(list(...)), "rq_partial")
-  step <- wrapr::wrap_fname_S3(fn_name = fn_name,
-                               fn_package = fn_package,
-                               arg_name = arg_name,
-                               args = args)
+  display_form <- format(step)
   nd <- non_sql_node(source = source,
                      f_db = NULL,
                      f_df = f_eval_partial_step,
@@ -43,52 +41,7 @@ rq_partial <- function(source, fn_name,
                      incoming_table_name = "fk_name_1",
                      outgoing_table_name = "fk_name_1",
                      columns_produced = columns_produced,
-                     display_form = paste0(fn_package, "::", fn_name),
-                     orig_columns = FALSE,
-                     temporary = TRUE,
-                     check_result_details = check_result_details,
-                     env = env)
-  nd$partial_step <- step
-  nd
-}
-
-
-#' Wrap a function name and arguments as an rqdatatable stage.
-#'
-#' @param source incoming relop pipeline.
-#' @param fn a function to wrap.
-#' @param ... force later arguments to be taken by name.
-#' @param arg_name name for remaining argument.
-#' @param args list of function argument values
-#' @param columns_produced columns of this node's result.
-#' @param check_result_details logical, if TRUE enforce result type and columns.
-#' @param env environment to work in.
-#' @return wrapped function
-#'
-#' @seealso \code{\link{applyto}}
-#'
-#' @export
-#'
-rq_partialf <- function(source, fn = NULL,
-                        ...,
-                        arg_name = '', args = list(),
-                        columns_produced = NULL,
-                        check_result_details = TRUE,
-                        env = parent.frame()) {
-  force(columns_produced)
-  force(env)
-  wrapr::stop_if_dot_args(substitute(list(...)), "rq_partialf")
-  step <- wrapr::wrap_function_S3(fn = fn,
-                                  arg_name = arg_name,
-                                  args = args)
-  nd <- non_sql_node(source = source,
-                     f_db = NULL,
-                     f_df = f_eval_partial_step,
-                     f_dt = NULL,
-                     incoming_table_name = "fk_name_1",
-                     outgoing_table_name = "fk_name_1",
-                     columns_produced = columns_produced,
-                     display_form = "function",
+                     display_form = display_form,
                      orig_columns = FALSE,
                      temporary = TRUE,
                      check_result_details = check_result_details,
