1a282399375225aac2f330d16b60b27f34f46972
John Mount <jmount@win-vector.com>
2018-11-24 08:20:08 -0800

    colon issue

---
+++ tests/testthat/test_colon.R
@@ -0,0 +1,11 @@
+
+library("rqdatatable")
+
+context("test_colon")
+
+test_that("test_colon.R", {
+
+  d <- data.frame(t = 1:2)
+  r <- d %.>% extend(., u = 4:5)
+  testthat::expect_equal(data.frame(t = 1:2, u = 4:5), r)
+})
